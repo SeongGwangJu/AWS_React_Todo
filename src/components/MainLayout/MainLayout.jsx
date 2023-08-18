1b89a367536bc3b171add61237945b3b6edad6e8
SeongGwangJu <seonggwangdev@gmail.com>
2023-08-18 10:27:09 +0900

    기본폴더세팅. 오늘자 노션 참조

---
+++ src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import React from 'react';
+
+function MainLayout(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default MainLayout;
---
+++ src/components/MainSidebar/MainSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainSidebar(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default MainSidebar;
